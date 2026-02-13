edb4fa9d7fc4f6570d2b2ce75efcb629cee519dc
Damilare Fasoranti <162375867+Darey247@users.noreply.github.com>
2026-02-13 14:20:06 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Orbitron', 'sans-serif'],
+      },
+      colors: {
+        stone: {
+          50: '#fafaf9',
+          100: '#f5f5f4',
+          200: '#e7e5e4',
+          300: '#d6d3d1',
+          400: '#a8a29e',
+          500: '#78716c',
+          600: '#57534e',
+          700: '#44403c',
+          800: '#292524',
+          900: '#1c1917',
+          950: '#0c0a09',
+        },
+        orange: {
+          500: '#f97316',
+          600: '#ea580c',
+          700: '#c2410c',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
